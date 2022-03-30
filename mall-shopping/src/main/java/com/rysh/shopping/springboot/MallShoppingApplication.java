4ca364000706924c90c29899b54c85ffb5a2ac2f
liuhuan <liuhuan@wsecar.com>
2022-03-30 16:39:30 +0800

    chore:first commit

---
+++ mall-shopping/src/main/java/com/rysh/shopping/springboot/MallShoppingApplication.java
@@ -0,0 +1,16 @@
+package com.rysh.shopping.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author liuhuan
+ * @date 2022/03/30 15:22
+ * @desc //todo
+ **/
+@SpringBootApplication
+public class MallShoppingApplication  {
+    public static void main(String[] args) {
+        SpringApplication.run(MallShoppingApplication.class, args);
+    }
+}
